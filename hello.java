de8508204f8b3651961c00a81327398304493e90
Md Shamim Sarder <shamimsrdr594@gmail.com>
2020-03-05 17:26:53 +0600

    Add a new method to display to Hello.java

--- hello.java
+++ hello.java
@@ -2,4 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("VS code is awesome");
     }
+    public static void display(String name){
+        System.out.println("Welcome"+ name);
+    }
 }
